6dcc3536443e080eadc3f437258969f22a7323a5
Emre <emrekurtt1655@gmail.com>
2020-12-25 20:23:45 +0100

    create: project template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid'
+import logo from'./virus.svg'
+ const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+    <CssBaseline />
+    <Container maxWidth="lg">
+      <Grid container>
+       
+      </Grid>
+    
+    </Container>
+  </React.Fragment>
+  )
 }
 
-export default App;
+export default App
